548730b4e9ca8dca2a26ce7f7cc70aeaa3e99def
ChronyxLaptop <ChronyxLaptop@Chronyx-Laptop>
2023-03-20 16:15:20 +0900

    2023-03-20
    박현준
    main : header를 JQuery로 불러 올 시 thymeleaf 적용안되는 부분 -> JQuery 사용 안하는 방법으로
    수정

--- LMS_PROJECT/src/main/java/com/project/lms/controller/HomeController.java
+++ LMS_PROJECT/src/main/java/com/project/lms/controller/HomeController.java
@@ -1,5 +1,15 @@
 package com.project.lms.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("name", "park");
+		return "index";
+	}
 }
