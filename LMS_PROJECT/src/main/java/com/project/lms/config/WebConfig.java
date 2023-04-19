3dd75dcc5bca3aa0dcfe67b239dbf42724b2e95c
jay-dokyeong <kjiblack1@gmail.com>
2023-04-19 10:58:23 +0900

    23.04.19 인터셉터 수정
    인터셉터 수정하였습니다.

--- LMS_PROJECT/src/main/java/com/project/lms/config/WebConfig.java
+++ LMS_PROJECT/src/main/java/com/project/lms/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		
-		String[] excludePathPatterns = {							// 인터셉터에서 제외할 URL패턴
+		String[] excludePathPatterns = {			// 인터셉터에서 제외할 URL패턴
             "/",
             "/members/join",
             "/members/login",
@@ -21,14 +21,18 @@ public class WebConfig implements WebMvcConfigurer {
             "/members/joinForm",
             "/members/loginForm",
             "/logout",
-            "/subject",
-            "/error",
+            "/subject/",
+            "/error",	
             "/css/**",
             "/js/**",
             "/footer.html",
             "/notification",
             "/readnotice/**",
-            "/fnq/",
+            "/readqna/**",
+//            "/writeQna",				이게 있으면 로그인 하지 않아도 등록이 되므로 페이지로 바로 이동했을 때, 로그인 페이지로 이동하도록 처리
+//            "/updateQna/**",			이게 있으면 로그인 하지 않아도 수정이 되므로 페이지로 바로 이동했을 때, 로그인 페이지로 이동하도록 처리
+            "/deleteQna/**",
+            "/fnq",
             "/introduce",
             "/policy",
             "/qna",
@@ -41,14 +45,6 @@ public class WebConfig implements WebMvcConfigurer {
 	            .order(1)											// 인터셉터 호출 순서(숫자가 낮을수록 먼저)
 	            .addPathPatterns("/**")								// 인터셉터 적용할 URL
 	            .excludePathPatterns(excludePathPatterns);
-	    
-	    
-//	    registry.addInterceptor(new CheckInterceptor())
-//		.order(2)
-//		.addPathPatterns("/**")
-//		.excludePathPatterns("/*.css", "/*.js");
-		
+	
 	}
-	
-	
 }
--- LMS_PROJECT/src/main/java/com/project/lms/controller/AssignmentController.java
+++ LMS_PROJECT/src/main/java/com/project/lms/controller/AssignmentController.java
@@ -62,31 +62,39 @@ public class AssignmentController {
 	@Value("${file.upload.path}")
 	private String uploadPath;	// 업드로 했을 때 저장될 위치 지정
 	
-	
-	@ModelAttribute("applyAssignment")
-	public MyLecture check(@PathVariable Long subject_no,
-			@SessionAttribute(name = "loginMember", required = false) Member loginMember,
-			HttpServletResponse response) throws IOException {
-		
-		MyLecture check = mylectureMapper.isMylectureExist(subject_no, loginMember.getMember_no());
-		if(check == null) {
+	// 인터셉터 대신 사용될 코드 Controller 내 메소드 실행전 실행된다.
+	// 수강신청을 했는지 확인하는 절차. 수강신청을 하지 않으면 강의를 볼 수 없다. 강의페이지에 들어오지 않도록 한다.
+	@ModelAttribute("applyLecture")
+	public MyLecture checkLecture(@PathVariable Long subject_no,
+								  @SessionAttribute(name = "loginMember", required = false) Member loginMember, 
+								  HttpServletResponse response) throws IOException {
+		log.info("pathsub: {}", subject_no);
+		log.info("member: {}", loginMember);
+
+		// 내강의에 정보가 있는지 없는지 확인하는 Query문
+		MyLecture isMylectureExist = mylectureMapper.isMylectureExist(subject_no, loginMember.getMember_no());
+		log.info("isExist:{}", isMylectureExist);
+		if (isMylectureExist == null) {
 			response.setContentType("text/html; charset=UTF-8");
 			PrintWriter out = response.getWriter();
-			out.println("<script>alert('로그인을 먼저 해주시기 바랍니다.'); location.href='/members/login'; </script>");
+			out.println("<script>alert('수강신청을 먼저 해주시기 바랍니다.'); location.href='/subject/" + subject_no
+					+ "/notification';</script>");
 			out.flush();
 		}
-		return check;
+		return isMylectureExist;
 	}
 	
 	// 기본경로
 	@GetMapping("{subject_no}/assignment")
-	public String goToAssign(@PathVariable Long subject_no, @RequestParam(defaultValue = "1") int page,
-			  @RequestParam(required = false) String title_part, Model model) {
-		log.info("title_part:{}", title_part);	// title_part는 검색용
+	public String goToAssign(@PathVariable Long subject_no, 
+							 @RequestParam(defaultValue = "1") int page,
+							 @RequestParam(required = false) String title_part, Model model) {
+		// title_part는 검색용
+		log.info("title_part:{}", title_part);	
 		
 		// 검색 조건 없을 때, null 출력. category 이름으로 총 갯수 검색
 		int total = assignmentMapper.getTotal(title_part, subject_no);
-		// 글이 없을 경우, total=0이되면 이상하니까 1로 처리.
+		// 글이 없을 경우, total=0 이되면 이상하니까 1로 처리.
 		if (total < 1) {
 			total = 1;
 		}
@@ -112,157 +120,146 @@ public class AssignmentController {
 	}
 
 	// 과제게시판 등록 폼으로 이동
-		@GetMapping("{subject_no}/assignment/write")
-		public String write(@PathVariable("subject_no") Long subject_no, Model model) {
-			// 과목 받아오는 subject 객체 생성
-			Subject subject = subjectMapper.findSubjectByNo(subject_no);
-			model.addAttribute("subject", subject);
-			// DTO 처리
-			model.addAttribute("write", new AssignmentWriteForm(subject_no));
-		//	model.addAttribute("updateAssign", new AssignmentUpdateForm(subject_no));
+	@GetMapping("{subject_no}/assignment/write")
+	public String write(@PathVariable("subject_no") Long subject_no, Model model) {
+		// 과목 받아오는 subject 객체 생성
+		Subject subject = subjectMapper.findSubjectByNo(subject_no);
+		model.addAttribute("subject", subject);
+		// DTO 처리
+		model.addAttribute("write", new AssignmentWriteForm(subject_no));
+		// model.addAttribute("updateAssign", new AssignmentUpdateForm(subject_no));
+		return "subject/assignment/write";
+	}
+	
+	// 글 등록
+	@PostMapping("{subject_no}/assignment/write")
+	public String write(@PathVariable("subject_no") Long subject_no,
+			@SessionAttribute(name = "loginMember", required = false) Member loginMember,
+			@Validated @ModelAttribute("write") AssignmentWriteForm write, BindingResult result, MultipartFile file) {
+		// 유효성 검사
+		if (result.hasErrors()) {
 			return "subject/assignment/write";
 		}
-	
-		// 글 등록
-		@PostMapping("{subject_no}/assignment/write")
-		public String write(@PathVariable("subject_no") Long subject_no,
-				@SessionAttribute(name = "loginMember", required = false) Member loginMember,
-				@Validated @ModelAttribute("write") AssignmentWriteForm write, BindingResult result,
-				MultipartFile file) {
-			// 유효성 검사
-			if (result.hasErrors()) {
-				return "subject/assignment/write";
-			}
-			
-			Subject subject = subjectMapper.findSubjectByNo(subject_no);
 
-			// 파일 업로드
-			log.info("file:{}", file);
-			log.info("file.getOriginalFileName():{}", file.getOriginalFilename());
-			log.info("file.getSize():{}", file.getSize());
+		Subject subject = subjectMapper.findSubjectByNo(subject_no);
 
-			AttachedFile savedFile = fileService.saveFile(file);
-			log.info("savedFile:{}", savedFile);
+		// 파일 업로드
+		log.info("file:{}", file);
+		log.info("file.getOriginalFileName():{}", file.getOriginalFilename());
+		log.info("file.getSize():{}", file.getSize());
+
+		AttachedFile savedFile = fileService.saveFile(file);
+		log.info("savedFile:{}", savedFile);
 
 		// DTO를 Entity로 변환
-			write.setWriter(loginMember.getMember_id());
-			Assignment assignment = write.toAssignment(write);
-			assignment.setOriginalfile(savedFile.getOriginalfile());	// 업로드 했던 것을 담아줘야 한다.
-			assignment.setSavedfile(savedFile.getSavedfile());
-			
-			log.info("assignment:{}", assignment);
-			assignmentMapper.writeAssignment(assignment);
-			
-			return "redirect:/subject/" + assignment.getSubject_no() + "/assignment";
-		}	
-	
-	// 읽기 페이지 이동
-		@GetMapping("{subject_no}/assignment/read/{assignment_no}")
-		public String read(@SessionAttribute(name = "loginMember", required = false) Member loginMember,
-				@RequestParam(defaultValue = "1") int page,
-				@PathVariable Long subject_no, @PathVariable Long assignment_no,
-				@RequestParam(required = false) String title_part, Model model) {
+		write.setWriter(loginMember.getMember_id());
+		Assignment assignment = write.toAssignment(write);
+		assignment.setOriginalfile(savedFile.getOriginalfile()); // 업로드 했던 것을 담아줘야 한다.
+		assignment.setSavedfile(savedFile.getSavedfile());
 
-			
-			// 글을 읽기 위한 객체 생성
-			Subject subject = subjectMapper.findSubjectByNo(subject_no);
-			Assignment readAssign = assignmentMapper.findAssignmentByNo(assignment_no);
-			model.addAttribute("subject", subject);
-			model.addAttribute("readAssign", readAssign);
-			
-			// 파일을 읽기 위한 객체 생성
-			List<Assignment> files = assignmentMapper.downloadFile(assignment_no);
-			log.info("assignment_no:{}", assignment_no);
-			log.info("subject", subject);
-			model.addAttribute("files", files);
-			
-			return "subject/assignment/read";
+		log.info("assignment:{}", assignment);
+		assignmentMapper.writeAssignment(assignment);
+
+		return "redirect:/subject/" + assignment.getSubject_no() + "/assignment";
+	}
+
+	// 읽기 페이지 이동
+	@GetMapping("{subject_no}/assignment/read/{assignment_no}")
+	public String read(@SessionAttribute(name = "loginMember", required = false) Member loginMember,
+			@RequestParam(defaultValue = "1") int page, @PathVariable Long subject_no, @PathVariable Long assignment_no,
+			@RequestParam(required = false) String title_part, Model model) {
+
+		// 글을 읽기 위한 객체 생성
+		Subject subject = subjectMapper.findSubjectByNo(subject_no);
+		Assignment readAssign = assignmentMapper.findAssignmentByNo(assignment_no);
+		model.addAttribute("subject", subject);
+		model.addAttribute("readAssign", readAssign);
+
+		// 파일을 읽기 위한 객체 생성
+		List<Assignment> files = assignmentMapper.downloadFile(assignment_no);
+		log.info("assignment_no:{}", assignment_no);
+		log.info("subject", subject);
+		model.addAttribute("files", files);
+
+		return "subject/assignment/read";
+	}
+
+	// 읽기 - 다운로드
+	@GetMapping("{subject_no}/assignment/read/{assignment_no}/download")
+	public ResponseEntity<Resource> read(@PathVariable Long subject_no, @PathVariable Long assignment_no, Model model)
+			throws MalformedURLException {
+
+		Assignment readAssign = assignmentMapper.findAssignmentByNo(assignment_no);
+		model.addAttribute("readAssign", readAssign);
+
+		String fullPath = uploadPath + readAssign.getSavedfile();
+		log.info("fullPath:{}", fullPath);
+
+		// 바디에 넣어줄 값 세팅(file: 서버에 저장된 파일의 경로)
+		UrlResource resource = new UrlResource("file:" + fullPath); // 경로 정보
+
+		// 헤더에 넣어줄 값 세팅 (attatched; filename="다운로드 받을 파일명")
+		String encodingFileName = UriUtils.encode(readAssign.getOriginalfile(), StandardCharsets.UTF_8); // 한글 인식 되도록 하게
+																											// 하는...
+		String contentDisposition = "attached; filename =\"" + encodingFileName + "\"";
+
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition).body(resource);
+	}
+
+	// 수정 페이지 이동
+	@GetMapping("{subject_no}/assignment/update/{assignment_no}")
+	public String update(@SessionAttribute(name = "loginMember", required = false) Member loginMember,
+			@PathVariable Long subject_no, @PathVariable Long assignment_no, Model model) {
+		// 사이드바에서 과목명을 보여주기 위한 과목 객체 등록
+		Subject subject = subjectMapper.findSubjectByNo(subject_no);
+		model.addAttribute("subject", subject);
+		Assignment updateAssign = assignmentMapper.findAssignmentByNo(assignment_no);
+
+		// 작성자 = 로그인 멤버가 같은 경우 찾기
+		if (updateAssign.getWriter().equals(loginMember.getMember_id())) {
+			model.addAttribute("updateAssign", updateAssign);
+			return "subject/assignment/update";
 		}
-	
-		
-		// 읽기 - 다운로드
-		@GetMapping("{subject_no}/assignment/read/{assignment_no}/download")
-		public ResponseEntity<Resource> read(@PathVariable Long subject_no, @PathVariable Long assignment_no,
-				 Model model) throws MalformedURLException{
-			
-			Assignment readAssign = assignmentMapper.findAssignmentByNo(assignment_no);
-			model.addAttribute("readAssign", readAssign);
-			
-			String fullPath = uploadPath + readAssign.getSavedfile();
-			log.info("fullPath:{}", fullPath);
-			
-			// 바디에 넣어줄 값 세팅(file: 서버에 저장된 파일의 경로)
-			UrlResource resource = new UrlResource("file:"+ fullPath);		// 경로 정보
-			
-			// 헤더에 넣어줄 값 세팅 (attatched; filename="다운로드 받을 파일명")
-			String encodingFileName = UriUtils.encode(readAssign.getOriginalfile(), StandardCharsets.UTF_8); // 한글 인식 되도록 하게 하는...
-			String contentDisposition = "attached; filename =\"" + encodingFileName + "\"";
-			
-			return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition).body(resource);
+		return "redirect:/subject/" + subject_no + "/assignment";
+	}
+
+	// DB 수정
+	@PostMapping("{subject_no}/assignment/update/{assignment_no}")
+	public String update(@SessionAttribute(name = "loginMember", required = false) Member loginMember,
+			@PathVariable Long subject_no, @PathVariable Long assignment_no, Model model,
+			@Validated @ModelAttribute("updateAssign") Assignment assignment, BindingResult result) {
+
+		if (result.hasErrors()) {
+			return "subject/assignment/update";
 		}
-				
-		// 수정 페이지 이동
-		@GetMapping("{subject_no}/assignment/update/{assignment_no}")
-		public String update(@SessionAttribute(name = "loginMember", required = false) Member loginMember,
-							 @PathVariable Long subject_no, @PathVariable Long assignment_no, 
-							 Model model) {
-			// 사이드바에서 과목명을 보여주기 위한 과목 객체 등록
-			Subject subject = subjectMapper.findSubjectByNo(subject_no);
-			model.addAttribute("subject", subject);
-			Assignment updateAssign = assignmentMapper.findAssignmentByNo(assignment_no);
-			
-			// 작성자 = 로그인 멤버가 같은 경우 찾기
-			if (updateAssign.getWriter().equals(loginMember.getMember_id())) {
-				model.addAttribute("updateAssign", updateAssign);
-				return "subject/assignment/update";
-			}
+		log.info("assignment:{}", assignment);
+		Assignment findAssignment = assignmentMapper.findAssignmentByNo(assignment.getAssignment_no());
+		// 작성자 = 로그인 멤버가 같은 경우 set
+		if (loginMember.getMember_id().equals(findAssignment.getWriter())) {
+			findAssignment.setAssignment_title(assignment.getAssignment_title());
+			findAssignment.setAssignment_contents(assignment.getAssignment_contents());
+			findAssignment.setScore(assignment.getScore());
+			assignmentMapper.updateAssignment(findAssignment);
+			log.info("수정 성공");
 			return "redirect:/subject/" + subject_no + "/assignment";
 		}
-			
-		// DB 수정
-		@PostMapping("{subject_no}/assignment/update/{assignment_no}")
-		public String update(@SessionAttribute(name = "loginMember", required = false) Member loginMember,
-				@PathVariable Long subject_no, @PathVariable Long assignment_no, Model model,
-				@Validated @ModelAttribute("updateAssign")Assignment assignment, BindingResult result) {
-			
-			if (result.hasErrors()) {
-				return "subject/assignment/update";
-			}
-			log.info("assignment:{}", assignment);
-			Assignment findAssignment = assignmentMapper.findAssignmentByNo(assignment.getAssignment_no());
-			// 작성자 = 로그인 멤버가 같은 경우 set
-			if (loginMember.getMember_id().equals(findAssignment.getWriter())) {
-				findAssignment.setAssignment_title(assignment.getAssignment_title());
-				findAssignment.setAssignment_contents(assignment.getAssignment_contents());
-				findAssignment.setScore(assignment.getScore());
-				assignmentMapper.updateAssignment(findAssignment);
-				log.info("수정 성공");
-				return "redirect:/subject/" + subject_no + "/assignment";
-			}
-				log.info("수정 실패했습니다.");
-				
-			return "redirect:/subject/" + subject_no + "/assignment/read/" + assignment_no;	// 수정 실패시 수정 전 페이지로
-		}
-	
-		
-		// 과제글 삭제
-		@PostMapping("{subject_no}/assignment/delete/{assignment_no}")
-		public String delete(@SessionAttribute(name = "loginMember", required = false) Member loginMember,
-				@PathVariable Long subject_no, @PathVariable Long assignment_no) {
+		log.info("수정 실패했습니다.");
 
-			// 로그인 멤버 = 작성자가 일치 할 시 삭제 시키기
-			Assignment findAssignment = assignmentMapper.findAssignmentByNo(assignment_no);
-			if (findAssignment != null && loginMember.getMember_id().equals(findAssignment.getWriter())) {
-				assignmentMapper.deleteAssignment(assignment_no);
-			}
-			return "redirect:/subject/"+subject_no+"/assignment/";
+		return "redirect:/subject/" + subject_no + "/assignment/read/" + assignment_no; // 수정 실패시 수정 전 페이지로
+	}
+
+	// 과제글 삭제
+	@PostMapping("{subject_no}/assignment/delete/{assignment_no}")
+	public String delete(@SessionAttribute(name = "loginMember", required = false) Member loginMember,
+			@PathVariable Long subject_no, @PathVariable Long assignment_no) {
+
+		// 로그인 멤버 = 작성자가 일치 할 시 삭제 시키기
+		Assignment findAssignment = assignmentMapper.findAssignmentByNo(assignment_no);
+		if (findAssignment != null && loginMember.getMember_id().equals(findAssignment.getWriter())) {
+			assignmentMapper.deleteAssignment(assignment_no);
 		}
-		
-		
-		
-		
-		
-		
-		
+		return "redirect:/subject/" + subject_no + "/assignment/";
+	}
+
 }
 
